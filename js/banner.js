47a3e7e24a3aefd7b8975c413d50446237c9b7b8
Carlos Eduardo <carloseduardorodrigues495@gmail.com>
2023-12-18 14:41:24 -0300

    atualizacao

--- js/banner.js
+++ js/banner.js
@@ -1,14 +1,14 @@
 function slide1(){
-	document.getElementById('meu-banner').src='img/banner1.jpg';
+	document.getElementById('banner1').src='./imagens/banner2.png';
 	setTimeout('slide2()', 3000);
 }
 
 function slide2(){
-	document.getElementById('meu-banner').src='img/banner2.jpg';
+	document.getElementById('banner2').src='./imagens/banner3.png';
 	setTimeout('slide3()', 3000);
 }
 
 function slide3(){
-	document.getElementById('meu-banner').src='img/banner3.jpg';
+	document.getElementById('banner3').src='./imagens/banner4.png';
 	setTimeout('slide1()', 3000);
 }
